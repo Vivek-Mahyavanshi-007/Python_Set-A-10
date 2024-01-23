7d4ec149ff7169120f352ed132d6f7f83848df8c
Vivek-Mahyavanshi-007 <157041563+Vivek-Mahyavanshi-007@users.noreply.github.com>
2024-01-23 21:01:39 +0530

    Add files via upload

---
+++ Practical--10.py
@@ -0,0 +1,15 @@
+fh = open('Output.txt', 'w')
+
+# Binary to 
+
+binary_number = input('Enter the number base 2: ')
+decimal_result = int(binary_number, 2)
+fh.write(f'Binary to Decimal: {decimal_result}\n')
+
+# Decimal to binary
+
+decimal_number = int(input('Enter the number from base 10: '))
+binary_result = bin(decimal_number)[2:]
+fh.write(f'Decimal to Binary: {binary_result}')
+
+fh.close()
